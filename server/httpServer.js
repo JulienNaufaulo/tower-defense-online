d38c5010e40939499d033eb6532294b44bb849d8
Julien Foucault <jul.foucault@gmail.com>
2016-01-05 18:56:55 +0100

    modif path

--- server/httpServer.js
+++ server/httpServer.js
@@ -21,7 +21,7 @@ exports.startServer = function startServer(port, path, callback) {
     // app.set('views', path.join(process.env.PWD, 'public'));
 
     app.get('/', function(req, res){
-        res.sendFile('index.html', { root: "/public/" });
+        res.sendFile('index.html', { root: process.env.PWD +"../public/" });
         // res.sendFile('index.html', { root: __dirname+"/" });
     });
 
