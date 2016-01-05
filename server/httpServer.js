537b8dbcbb3124a6e82fa0d3b1ac63337398a450
Julien Foucault <jul.foucault@gmail.com>
2016-01-05 19:41:04 +0100

    modif path

--- server/httpServer.js
+++ server/httpServer.js
@@ -15,8 +15,8 @@ exports.startServer = function startServer(port, path, callback) {
 
     process.env.PWD = process.cwd();
 
-    app.set('css', process.env.PWD, '../public/css');
-    app.use(express.static(__dirname + '/../public'));
+    app.set('css', __dirname, '/../public/css/');
+    app.use(express.static(__dirname + '/../public/'));
     // app.use(express.static(process.env.PWD + '../public/'));
 
     // app.set('views', path.join(process.env.PWD, 'public'));
